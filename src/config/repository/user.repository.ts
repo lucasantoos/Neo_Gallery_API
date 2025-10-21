c8fe5850cdb6de21696401429445bb77fcae7813
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-21 16:39:57 -0400

    <feat>: evoluindo API

--- src/config/repository/user.repository.ts
+++ src/config/repository/user.repository.ts
@@ -1,24 +1,24 @@
-import { PrismaClient, RoleUser } from "@prisma/client";
-import type { ICreateUserInterface } from "../../domain/interfaces/user.interface.js";
-import type { User } from "../../domain/entities/User.js";
+import { PrismaClient} from "@prisma/client";
+import type { ICreateUserInterface } from "../../domain/repository/user.interface.js";
+import type { CreateUserDTO } from "../../domain/dtos/create.user.dto.js";
+import type { IReturnUserDTO } from "../../domain/dtos/return.user.dto.js";
 
-export class UserRepository {
+export class UserRepository implements ICreateUserInterface {
     constructor(private prisma: PrismaClient){}
     
-    async create(user: User): Promise<void> {
-        await this.prisma.user.create({data:{
+    async create(user: CreateUserDTO): Promise<IReturnUserDTO> {
+      const newUser = await this.prisma.user.create({data:{
             nome: user.nome,
             email:user.email,
+            senha:user.senha,
+            avatarUrl: user.avataUrl,
             role: "ADMIN",
-            senha: user.senha,
-            avatarUrl:user.avataUrl,
-            data: new Date(),
-            imagem:{
-                connect:{
-                    userid: user.GetId()
-                }
-            }
         }})
+        return {
+            id:newUser.id,
+            email: user.email,
+            nome: user.nome
+        }
     }
 
     async findAll(email: string): Promise<any> {
--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,30 +1,27 @@
+import type { Email } from "../value-objects/email.validator.js"
+
 export class User{
     constructor(
         private readonly id: number,
         public nome: string,
-        public  email: string,
+        public  email: Email,
         public  senha: string,
         public avataUrl: string,
         public role: string,
         public  data: Date
     ){}
 
-    GetId(){
-        return this.id
-    }
-
     addName(nome: string):void{
         if(nome.length <= 1) throw new Error("O nome deve ser válido")
             this.nome = nome
     }
 
-    addEmail(email:string): void{
-        if(email.length <= 5 || !email.includes("@,.")) throw new Error("O formato de email deve ser valido")
-
-    }
-
     addSenha(senha:string):void{
         if(senha.length <= 5)throw new Error("A senha precisa ter mais que 5 caracteres")
 
+    }
+
+    get(email:string): void{
+        this.email.addEmail(email)
     }
 }
