4382b0cadc19f7f2f92bb3a576d83ca27a8860ce
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-21 16:39:57 -0400

    <feat>: evoluindo API

--- src/domain/repository/user.interface.ts
+++ src/domain/repository/user.interface.ts
@@ -1,8 +1,9 @@
 import { User } from "../entities/User.js"
 import  type {CreateUserDTO } from "../dtos/create.user.dto.js"
+import type { IReturnUserDTO } from "../dtos/return.user.dto.js"
 
 export interface ICreateUserInterface{
-    create(user: CreateUserDTO): Promise<void>
+    create(user: CreateUserDTO): Promise<IReturnUserDTO>
     find(id: number): Promise<User>
     findAll(email:string): Promise<any>
     delete(id: number):Promise<void>
