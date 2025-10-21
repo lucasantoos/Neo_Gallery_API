7bd4a3f401574a49d24f70579e43585a2dcf782d
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-21 16:15:03 -0400

    <feat>: add controller and add DTO's

---
+++ src/domain/dtos/create.user.dto.ts
@@ -0,0 +1,9 @@
+export interface CreateUserDTO{
+ nome: string,
+ email: string,
+ senha: string,
+ avataUrl: string,
+ role: string
+}
+
+//Create DTO's of Inputs and Outputs
---
+++ src/domain/dtos/return.user.dto.ts
@@ -0,0 +1,5 @@
+export interface IReturnUserDTO{
+    id: number,
+    nome: string,
+    email: string,
+}
