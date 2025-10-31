7d6b2b7ea8e14d7d7c4669fa1215255cf38d00ed
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-30 23:04:15 -0300

    refatoring

--- src/application/controller/image.controller.ts
+++ src/application/controller/image.controller.ts
@@ -3,10 +3,11 @@ import type { ImageService } from "../use-case/imagem.service.js";
 export class ImageController{
     constructor(private ImageService : ImageService){}
     
-    public Image = (req:any, rep:any)=>{
+    public Image = async (req:any, rep:any)=>{
         try {
+            const {originalname} = req.file
            const {titulo, url, publico, data, userID} = req.body
-           const imagem = this.ImageService.createImagem({titulo, data, publico, url, userID})
+           const imagem = await this.ImageService.createImagem({titulo: originalname, data, publico, url, userID})
            console.log(req.file)
            return rep.status(201).send(imagem)
         } catch (e:any) {
